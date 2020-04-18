c029f66fdbf0ac9f9f20dcbbf866c76bea6ab78e
carflor <cfdeclet@gmail.com>
2020-04-17 20:55:15 -0600

    Class arranged

---
+++ src/Booking.js
@@ -0,0 +1,11 @@
+class Booking {
+  constructor(id, userId, date, roomNumber, roomServiceCharges) {
+    this.id = id;
+    this.userId = userId;
+    this.date = date;
+    this.roomNumber = roomNumber;
+    this.roomServiceCharges = roomServiceCharges;
+  }
+}
+
+export default Booking;
