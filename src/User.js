ad11ff00c35f6018a200f70de8c1e8816391234b
carflor <cfdeclet@gmail.com>
2020-04-18 14:59:27 -0600

    Removed booking history method and added book room method

--- src/User.js
+++ src/User.js
@@ -1,23 +1,34 @@
+import Booking from "./Booking";
+
 class User {
-  constructor(reservations, rooms) {
+  constructor(id, name, reservations, rooms) {
     this.id = id;
     this.name = name;
+    // list of reservations past / present / future
     this.reservations = reservations;
+    // list of rooms available for booking
     this.rooms = rooms;
   }
 
-  bookingHistory() {
-    // this works if the data is an array of objects
-    return reservations.filter(reservation => {
-      return reservation.userID === this.id;
-    })
-  }
+  // MOVED THIS TO MANAGER CLASS
+  // bookingHistory() {
+  //   // this works if the data is an array of objects
+  //   return reservations.filter(reservation => {
+  //     return reservation.userID === this.id;
+  //   })
+  // }
 
-  bookRoom() {
-    // should make a POST to update the rooms available
+  bookRoom(booking) {
+    let bookedRoom = new Booking(booking)
+    // instantiate a booking 
+    this.reservations.push(bookedRoom)
+    // adds a reservations to the array
   }
 
   unbookRoom() {
+    // this should find a booking in the 
+    // reservations array and remove it 
+    
     // should delete fetch the future reservation
   }
 }
