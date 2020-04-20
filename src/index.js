fa30c84139bf95ad43afee6aee402ccbda494f95
carflor <cfdeclet@gmail.com>
2020-04-20 14:00:42 -0600

    Added display of customer reservation data functionality

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ function loadCustomer(customerId, manager) {
   let guest = []
   let guestRooms = manager.rooms[0]
   let guestReservations = manager.bookingHistoryById(customerId, reservationData)
+  today = moment().format("YYYY-MM-DD").split('-').join('/')
   manager.users[0].forEach(user => {
     if (user.id === customerId) {
       guest.push(user)
@@ -56,6 +57,42 @@ function loadCustomer(customerId, manager) {
 function displayCustomer(customer) {
   $('.guest-name').text(customer.name)
   $('.points-message').text(`You currently have ${customer.rewardPoints().toFixed(0)} Reward Points!`)
+
+  let upcomingReservations = [];
+  customer.reservations.forEach(reservation => {
+    if (reservation.date >= today) {
+      upcomingReservations.push(reservation)
+    }
+  })
+  reservedRoomDetails(customer, upcomingReservations)
+}
+
+function displayCustomerReservations(customer, upcomingReservations, matchRoom) {
+  // console.log('room reserved', matchRoom)
+  if (upcomingReservations.length >= 1) {
+    $('.reservations-container').prepend(`
+    <section class="current-reservation">
+    <p class="reservation-date">${upcomingReservations[0].date}</p>
+    <p>Room # ${upcomingReservations[0].roomNumber}</p>
+    <p>${matchRoom[0].roomType.toUpperCase()}</p>
+    <p>Beds: ${matchRoom[0].numBeds} ${matchRoom[0].bedSize.toUpperCase()} </p>
+    <span>Confirmation #: ${upcomingReservations[0].id}</span>
+    </section>`)
+  } else {
+    $('.reservations-container').prepend(`<p class="no-reservations">No reservations found at this moment</p>`)
+  }
+}
+
+function reservedRoomDetails(customer, upcomingReservations) {
+  let matchRoom;
+  if (upcomingReservations.length >= 1) {
+    matchRoom = customer.rooms.filter(room => {
+      return upcomingReservations[0].roomNumber === room.number || 0
+    }) 
+  } else {
+    matchRoom = 0;
+  }
+  displayCustomerReservations(customer, upcomingReservations, matchRoom)
 }
 
 // LOG IN VALIDATION
