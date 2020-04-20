7461b001529e141cc5a6b61b72f7f3d06b9149b0
carflor <cfdeclet@gmail.com>
2020-04-20 11:05:59 -0600

    Added calculation for total reward points per guest

--- src/User.js
+++ src/User.js
@@ -4,19 +4,11 @@ class User {
   constructor(id, name, reservations, rooms) {
     this.id = id;
     this.name = name;
-    // list of reservations past / present / future
-    this.reservations = reservations;
     // list of rooms available for booking
     this.rooms = rooms;
+    // list of reservations past / present / future
+    this.reservations = reservations;
   }
-
-  // MOVED THIS TO MANAGER CLASS
-  // bookingHistory() {
-  //   // this works if the data is an array of objects
-  //   return reservations.filter(reservation => {
-  //     return reservation.userID === this.id;
-  //   })
-  // }
 
   bookRoom(booking) {
     let bookedRoom = new Booking(booking)
@@ -25,10 +17,20 @@ class User {
     // adds a reservations to the array
   }
 
+  rewardPoints() {
+    return this.reservations.reduce((acc, reservation) => {  
+      this.rooms.forEach(room => {
+        if (reservation.roomNumber === room.number) {
+          acc += room.costPerNight
+        }
+      })
+      return acc
+    }, 0)
+  }
+
   unbookRoom() {
     // this should find a booking in the 
     // reservations array and remove it 
-    
     // should delete fetch the future reservation
   }
 }
